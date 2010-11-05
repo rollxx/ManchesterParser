d0bba27bd58b3f075331bfe2f4b0c9a762a69888
rollxx <gravatar@rollxx.com>
2010-11-05 07:43:05 +0100

    file for copying generated parser

---
+++ move.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DEST=/Applications/MAMP/htdocs/myow/libraries/Erfurt/Erfurt/Syntax
+SOURCE=src
+
+cp $SOURCE/Erfurt_Syntax_ManchesterLexer.php $DEST/ManchesterLexer.php
+cp $SOURCE/Erfurt_Syntax_ManchesterParser.php $DEST/ManchesterParser.php
+cp $SOURCE/Erfurt_Syntax_Manchester_ManchesterTokenizer.php $DEST/Manchester/ManchesterTokenizer.php
